51d9f0536474601aed650724db2b39c7179a3c4f
Aayush Shukla <oreus.aks@gmail.com>
2023-06-20 17:39:37 +0530

    Created an activity to handle user authentication

---
+++ app/src/main/java/com/a2ys/conversa/activities/UserAuthenticationActivity.kt
@@ -0,0 +1,20 @@
+package com.a2ys.conversa.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a2ys.conversa.R
+import com.a2ys.conversa.databinding.ActivityMainBinding
+import com.a2ys.conversa.databinding.ActivityUserAuthenticationBinding
+
+class UserAuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserAuthenticationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserAuthenticationBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
