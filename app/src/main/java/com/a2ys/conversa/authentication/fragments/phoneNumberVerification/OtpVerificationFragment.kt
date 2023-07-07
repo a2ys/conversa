907a2b5e4eeba7d56230325727e3496ddf0a385b
Aayush Shukla <oreus.aks@gmail.com>
2023-07-07 22:22:30 +0530

    Moved the file and removed unused import

--- app/src/main/java/com/a2ys/conversa/authentication/fragments/phoneNumberVerification/OtpVerificationFragment.kt
+++ app/src/main/java/com/a2ys/conversa/authentication/fragments/phoneNumberVerification/OtpVerificationFragment.kt
@@ -1,11 +1,10 @@
-package com.a2ys.conversa.fragments.authentication
+package com.a2ys.conversa.authentication.fragments.phoneNumberVerification
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.a2ys.conversa.R
 import com.a2ys.conversa.databinding.FragmentOtpVerificationBinding
 
 class OtpVerificationFragment : Fragment() {
