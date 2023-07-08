f29f5a9bb28b447f33f0b12b66c8ca3d3864b095
Aayush Shukla <oreus.aks@gmail.com>
2023-07-08 18:34:44 +0530

    Created the fragment for Signing Up with Email

---
+++ app/src/main/java/com/a2ys/conversa/authentication/fragments/emailVerification/SignUpFragment.kt
@@ -0,0 +1,23 @@
+package com.a2ys.conversa.authentication.fragments.emailVerification
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.a2ys.conversa.R
+import com.a2ys.conversa.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
