1fdf058783b43dbb316b3b3f50efbe530b976e5c
Aayush Shukla <oreus.aks@gmail.com>
2023-07-11 09:45:37 +0530

    Created an activity for loading screen

---
+++ app/src/main/java/com/a2ys/conversa/authentication/activities/InfoActivity.kt
@@ -0,0 +1,12 @@
+package com.a2ys.conversa.authentication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a2ys.conversa.R
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
