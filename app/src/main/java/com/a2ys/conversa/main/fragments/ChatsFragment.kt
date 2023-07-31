24e15ee7d19744b9b3f0d6677e1256302431df18
Aayush Shukla <oreus.aks@gmail.com>
2023-07-31 08:52:22 +0530

    Removed unused import

--- app/src/main/java/com/a2ys/conversa/main/fragments/ChatsFragment.kt
+++ app/src/main/java/com/a2ys/conversa/main/fragments/ChatsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.a2ys.conversa.R
 import com.a2ys.conversa.databinding.FragmentChatsBinding
 
 class ChatsFragment : Fragment() {
