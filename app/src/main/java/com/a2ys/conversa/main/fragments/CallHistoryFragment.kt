745af9d0df1742f67dc5682db6e3f6eca579fda1
Aayush Shukla <oreus.aks@gmail.com>
2023-09-12 17:59:26 +0530

    Removed unused imports

--- app/src/main/java/com/a2ys/conversa/main/fragments/CallHistoryFragment.kt
+++ app/src/main/java/com/a2ys/conversa/main/fragments/CallHistoryFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.a2ys.conversa.R
 import com.a2ys.conversa.databinding.FragmentCallHistoryBinding
-import com.a2ys.conversa.databinding.FragmentChatsBinding
 
 class CallHistoryFragment : Fragment() {
 
