f9b06272867bad67fe1da9ad57d2cda905e2ffc7
Aayush Shukla <oreus.aks@gmail.com>
2023-11-16 13:24:39 +0530

    Fixed the issue where the contents were static; navigation bar was not working

--- app/src/main/java/com/a2ys/conversa/main/activities/MainActivity.kt
+++ app/src/main/java/com/a2ys/conversa/main/activities/MainActivity.kt
@@ -2,16 +2,23 @@ package com.a2ys.conversa.main.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.a2ys.conversa.R
 import com.a2ys.conversa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_navigation)
+        val navController = navHostFragment!!.findNavController()
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
