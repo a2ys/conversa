9b3474a61cf19d446fc1c71420c95d08b03f2059
Aayush Shukla <oreus.aks@gmail.com>
2024-03-03 03:40:33 +0530

    Improved performance on the landing page to decrease startup time

--- app/src/main/java/dev/a2ys/conversa/landingpage/activities/LandingPageActivity.kt
+++ app/src/main/java/dev/a2ys/conversa/landingpage/activities/LandingPageActivity.kt
@@ -3,62 +3,67 @@ package dev.a2ys.conversa.landingpage.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.a2ys.conversa.R
-import dev.a2ys.conversa.authentication.activities.InfoActivity
-import dev.a2ys.conversa.authentication.activities.UserAuthenticationActivity
 import com.a2ys.conversa.databinding.ActivityLandingPageBinding
-import dev.a2ys.conversa.main.activities.MainActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import dev.a2ys.conversa.authentication.activities.InfoActivity
+import dev.a2ys.conversa.authentication.activities.UserAuthenticationActivity
+import dev.a2ys.conversa.main.activities.MainActivity
 
 class LandingPageActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var uid: String
     private lateinit var database: DatabaseReference
     private lateinit var binding: ActivityLandingPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page)
-
         binding = ActivityLandingPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
+        database = FirebaseDatabase.getInstance().reference
 
+        checkUserAuthentication()
+    }
+
+    private fun checkUserAuthentication() {
         val currentUser = auth.currentUser
-
         if (currentUser != null) {
-            val user = Firebase.auth.currentUser
-            user?.let {
-                uid = it.uid
-            }
-
-            database = FirebaseDatabase.getInstance().reference
-
-            database.child("registeredUsers").child(uid).get().addOnCompleteListener {
-                if (it.isSuccessful) {
-                    if (it.result.exists()) {
-                        startActivity(Intent(applicationContext, MainActivity::class.java))
-                        finish()
-                    } else {
-                        startActivity(Intent(applicationContext, InfoActivity::class.java))
-                        finish()
-                    }
-                } else {
-                    Snackbar.make(findViewById(R.id.container), "Please contact the developer!", Snackbar.LENGTH_SHORT)
-                        .setAction("Got it") {}
-                        .show()
-                }
-            }
+            navigateUser(currentUser.uid)
         } else {
-            startActivity(Intent(applicationContext, UserAuthenticationActivity::class.java))
-            finish()
+            navigateToAuthenticationActivity()
         }
+    }
+
+    private fun navigateUser(uid: String) {
+        database.child("registeredUsers").child(uid).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                if (task.result.exists()) {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                    finish()
+                } else {
+                    startActivity(Intent(applicationContext, InfoActivity::class.java))
+                    finish()
+                }
+            } else {
+                showErrorSnackbar()
+            }
+        }
+    }
+
+    private fun navigateToAuthenticationActivity() {
+        startActivity(Intent(applicationContext, UserAuthenticationActivity::class.java))
+        finish()
+    }
+
+    private fun showErrorSnackbar() {
+        Snackbar.make(binding.root, "Please contact the developer!", Snackbar.LENGTH_SHORT)
+            .setAction("Got it") {}
+            .show()
     }
 }
