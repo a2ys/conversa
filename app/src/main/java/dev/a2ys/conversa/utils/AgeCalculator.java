b5935582fc0e377b816e68386384bd72337e8cf3
Aayush Shukla <oreus.aks@gmail.com>
2024-03-03 23:46:41 +0530

    Optimized the Age Calculator Script

--- app/src/main/java/dev/a2ys/conversa/utils/AgeCalculator.java
+++ app/src/main/java/dev/a2ys/conversa/utils/AgeCalculator.java
@@ -1,31 +1,27 @@
 package dev.a2ys.conversa.utils;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class AgeCalculator {
-    public int getAge(int year, int month, int dayOfMonth) {
-        int age = getDate()[0] - year;
 
-        if (getDate()[1] - month < 0) {
+    private final int currentYear;
+    private final int currentMonth;
+    private final int currentDayOfMonth;
+
+    public AgeCalculator() {
+        LocalDateTime now = LocalDateTime.now();
+        currentYear = now.getYear();
+        currentMonth = now.getMonthValue();
+        currentDayOfMonth = now.getDayOfMonth();
+    }
+
+    public int getAge(int year, int month, int dayOfMonth) {
+        int age = currentYear - year;
+
+        if (currentMonth - month < 0 || (currentMonth == month && currentDayOfMonth - dayOfMonth < 0)) {
             age--;
-        } else if (getDate()[1] == month) {
-            if (getDate()[2] - dayOfMonth < 0) {
-                age--;
-            }
         }
 
         return age;
-    }
-
-    private int[] getDate() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        String dateTimeString = dtf.format(now).split(" ")[0];
-
-        String[] dateTimeStringList = dateTimeString.split("/");
-
-        return new int[]{Integer.parseInt(dateTimeStringList[0]), Integer.parseInt(dateTimeStringList[1]), Integer.parseInt(dateTimeStringList[2])};
     }
 }
